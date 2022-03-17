56bdf9397ca98838356586e2cb45a30fd7069320
JuanParedes190402 <92234332+JuanParedes190402@users.noreply.github.com>
2022-03-16 20:41:51 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,56 +1,63 @@
-const texts = document.querySelector('.texts');
-const ala1 = document.getElementById("ala1");
-const ala2 = document.getElementById("ala2");
-const ala3 = document.getElementById("ala3");
-const ala4 = document.getElementById("ala4");
-const ala5 = document.getElementById("ala5");
-
-window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
 const recognition = new SpeechRecognition();
 recognition.interimResults = true;
 
-recognition.addEventListener('result', (e)=>{
-    const text = Array.from(e.results)
+let ala1 = document.getElementById('ala1');
+let ala2 = document.getElementById('ala2');
+let ala3 = document.getElementById('ala3');
+let ala4 = document.getElementById('ala4');
+let ala5 = document.getElementById('ala5');
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+recognition.addEventListener('result', (e) => {
+  const text = Array.from(e.results)
     .map(result => result[0])
     .map(result => result.transcript)
     .join('');
-console.log(text);
-  if(e.results[0].isFinal){
-    if (text.includes('Enciende foco 1') || text.includes('foco 1') || text.includes('enciende foco 1')) {            
-      ala1.style.background = "url(bulb_on.jpg)";
-    }
-    if (text.includes("Apaga foco uno") || text.includes('apaga foco 1')) {
-      ala1.style.background = "url(bulb_off.jpg)";
-    }
-    if (text.includes("Enciende foco 2") || text.includes('foco 2') || text.includes('enciende foco 2')) {      
-      ala2.style.background = "url(bulb_on.jpg)";
-    }
-    if (text.includes("Apaga foco dos") || text.includes('apaga foco 2')) {
-      ala2.style.background = "url(bulb_off.jpg)";
-    }
-    if (text.includes("Enciende foco 3") || text.includes('foco 3') || text.includes('enciende foco 3')) {      
-      ala3.style.background = "url(bulb_on.jpg)";
-    }
-    if (text.includes("Apaga foco tres") || text.includes('apaga foco 3')) {
-      ala3.style.background = "url(bulb_off.jpg)";
-    }
-    if (text.includes("Enciende foco 4") || text.includes('foco 4') || text.includes('enciende foco 4')) {      
-      ala4.style.background = "url(bulb_on.jpg)";
-    }
-    if (text.includes("Apaga foco cuatro") || text.includes('apaga foco 4')) {
-      ala4.style.background = "url(bulb_off.jpg)";
-    }
-    if (text.includes("Enciende foco 5") || text.includes('foco 5') || text.includes('enciende foco 5')) {      
-      ala5.style.background = "url(bulb_on.jpg)";
-    }
-    if (text.includes("Apaga foco cinco") || text.includes('apaga foco 5')) {
-      ala5.style.background = "url(bulb_off.jpg)";
-    }
 
+  if (e.results[0].isFinal) {
+    console.log(text);
+    if (text.includes('luz 1') || text.includes('Luz 1')) {
+      if(ala1.checked == 0){
+        ala1.checked = 1;
+      }else{
+        ala1.checked = 0;
+      }
+    }
+    if (text.includes('luz 2') || text.includes('Luz 2')) {
+      if(ala2.checked == 0){
+        ala2.checked = 1;
+      }else{
+        ala2.checked = 0;
+      }
+    }
+    if (text.includes('luz 3') || text.includes('Luz 3')) {
+      if(ala3.checked == 0){
+        ala3.checked = 1;
+      }else{
+        ala3.checked = 0;
+      }
+    }
+    if (text.includes('luz 4') || text.includes('Luz 4')) {
+      if(ala4.checked == 0){
+        ala4.checked = 1;
+      }else{
+        ala4.checked = 0;
+      }
+    }
+    if (text.includes('luz 5') || text.includes('Luz 5')) {
+      if(ala5.checked == 0){
+        ala5.checked = 1;
+      }else{
+        ala5.checked = 0;
+      }
+    }
+    
   }
 });
-recognition.addEventListener('end', ()=>{
+
+
+recognition.addEventListener('end', () => {
   recognition.start();
 })
 
